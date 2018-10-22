ffb9d7e074380aed0929855c9cafec619e700df2
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-22 13:41:47 -0700

    finalized giphy app

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const exphbs = require('express-handlebars');
+const http = require('http');
+const giphy = require('giphy-api')();
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.get('/hello-girl', (req, res) => {
+    res.send('Hello girl')
+})
+
+app.get('/hello-gif', (req, res) => {
+    let gifUrl = "http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif"
+    res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/greetings/:name', (req, res) => {
+    let name = req.params.name;
+    res.render('greetings', {name: name})
+})
+
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+    console.log({gifs: response.data})
+  });
+});
+
+app.listen(3000, function () {
+    console.log('GIf Search listening on localhost:3000!')
+})
